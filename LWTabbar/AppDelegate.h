aa20cda25912f443d01a824c091e6846e39d0b11
Liu Wei <liuwei@lwimac.local>
2016-09-05 16:22:35 +0800

    Initial Commit

---
+++ LWTabbar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LWTabbar
+//
+//  Created by Liu Wei on 16/9/5.
+//  Copyright © 2016年 Liu Wei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
